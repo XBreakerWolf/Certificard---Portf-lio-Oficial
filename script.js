2e68b2bd65ed92107876a2838853750dab19bd7a
Isaías <113859366+XBreakerWolf@users.noreply.github.com>
2022-09-21 18:45:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function alterarTema() {
+  document.body.classList.toggle("dark");
+  //   body: retoma os conteúdos da tag <body> do HTML
+  //   classList: atribui uma classe ao conteúdo da tag <body> que poderá ser retomada no CSS
+  //   toggle: remete à alternância do botão "Alterar Tema"
+  //   ("dark"): é a segunda classe que estará na alternância
+}
